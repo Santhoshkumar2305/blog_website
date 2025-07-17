bd0bc4d36925bb23f8faaac3ae8e81d5343229ae
jeevananthams3113 <santhoshkumar.s2023cse@sece.ac.in>
2025-07-17 22:11:05 +0530

    latest updated code

--- app/(admin)/set/page.js
+++
@@ -1,9 +0,0 @@
-const Set = () => {
-  return (
-    <div>
-        <h1>Seller's settings page</h1>
-    </div>
-  )
-}
-
-export default Set;
--- app/(user)/settings/page.js
+++
@@ -1,9 +0,0 @@
-const Settings = () => {
-  return (
-    <div>
-        <h1>Buyer's Settings Page</h1>
-    </div>
-  )
-}
-
-export default Settings
